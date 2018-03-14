475245996ce525085c9d303c97a4cd7425056b4c
zhengcheng <gzzhengcheng@corp.netease.com>
2018-03-14 12:28:07 +0800

    update image input code example.

--- image_input/image_input.py
+++ image_input/image_input.py
@@ -2,9 +2,9 @@
 
 import numpy as np
 import tensorflow as tf
+from tensorflow.python.platform import gfile
 
 def create_image_input_tensor(
-    image_file_path,
     input_depth,
     target_with,
     target_height):
@@ -14,8 +14,9 @@ def create_image_input_tensor(
         target_width: int, Desired image width for later process.
         target_height: int, Desired image height for later process.
 
-    Return:
-        Tensor which will output the processed image.
+    Returns:
+        resized_image: tensor which will output the processed image.
+        jpeg_data: placeholder for raw image data.
     """
 
     jpeg_data = tf.placeholder(tf.string, name='decode_jpeg_input')
@@ -30,4 +31,24 @@ def create_image_input_tensor(
     resize_shape_as_int = tf.cast(resize_shape, dtype=tf.int32)
     resized_image = tf.image.resize_bilinear(decoded_image_4d, resize_shape_as_int)
 
-    return resized_image
+    return resized_image, jpeg_data
+
+if __name__ == '__main__':
+    # read image
+    image_file_path = '/replace/to/your/own/test.jpg'
+    image_data = gfile.FastGFile(image_file_path, 'rb').read()
+
+    # create image input tensor
+    target_width = 299
+    target_height = 299
+    input_depth = 3
+    image_input_tensor, image_data_placeholder = create_image_input_tensor(input_depth, target_width, target_height)
+
+    sess = tf.Session()
+
+    # run the tensor. For feed_dict arg, you can also use: {'decode_jpeg_input:0': image_data}
+    resized_image = sess.run(image_input_tensor, feed_dict={image_data_placeholder: image_data})
+
+    # log result
+    print "result shape: ", sess.run(tf.shape(resized_image))
+    sess.run(resized_image)
